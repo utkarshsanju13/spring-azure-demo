00729539b2cfdaa4a9511926fb771f9c220dafc5
utkarshsanju13 <utkarshsanju13@gmail.com>
2023-02-19 19:50:12 +0530

    simple get api

--- src/main/java/com/azure/springazuredemo/SpringAzureDemoApplication.java
+++ src/main/java/com/azure/springazuredemo/SpringAzureDemoApplication.java
@@ -2,9 +2,17 @@ package com.azure.springazuredemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringAzureDemoApplication {
+	
+	@GetMapping("/message")
+	public String message() {
+		return "Congrats your app deployed successfully in azure";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAzureDemoApplication.class, args);
